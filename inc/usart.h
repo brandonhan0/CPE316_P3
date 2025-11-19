11d5c084832e6da2089a2b4a8ce4acd765019cfc
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2025-11-19 15:19:16 -0800

    Add USART header file with initialization and write functions

---
+++ inc/usart.h
@@ -0,0 +1,11 @@
+#ifndef USART_H
+
+#define USART_H
+
+#include "stm32l4xx_hal.h" // so we can use HAL functions
+#include <stdint.h>
+
+void USART_init(void);
+void USART_write(void); // change
+
+#endif
