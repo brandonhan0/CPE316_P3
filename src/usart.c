5f9c95b89318c2c2d2f60111e7a5b6ba074382eb
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2025-11-20 15:47:13 -0800

    usart

--- src/usart.c
+++ src/usart.c
@@ -1,6 +1,11 @@
 #include "usart.h"
 
-void USART_PINS_init(void){
+
+static inline uint32_t usart_brr(uint32_t pclk_hz, uint32_t baud){
+    return (pclk_hz + (baud/2U)) / baud; // rounded
+}
+
+void USART_init(uint32_t pclk_hz, uint32_t baud){
 
 	RCC->AHB2ENR  |= (RCC_AHB2ENR_GPIOAEN);
 	RCC->APB1ENR1 |= (1 << 17);
@@ -18,8 +23,22 @@ void USART_PINS_init(void){
 	GPIOA->AFR[0] &= ~((0xF << 8) | (0xF << 12)); // clear
 	GPIOA->AFR[0] |=  ((0x7 << 8) | (0x7 << 12)); // AF7
 	
+	USART2->BRR  = usart_brr(pclk_hz, baud);
+	USART2->CR1 &= ~USART_CR1_UE; // cant have this on during config
+	USART2->CR1 = 0; // default config
+    USART2->CR2 = 0; // stop bit
+    USART2->CR3 = 0; // no hw flow
+    USART2->CR1 = (1 << 3) | (1 << 2); // should enable tx we dont need rx maybe for programming idk its on now though
+    
+    USART2->CR1 |= (1 << 0); // en b1
 }
 
-void USART_write(char* data){ // i would rather use HAL here bruh aint nobody wanna do memory shit
-    HAL_UART_Transmit(&huart2, (uint8_t*)data, strlen(data), HAL_MAX_DELAY);
+void USART_write(const char *s){
+    const uint8_t *p = (const uint8_t *)s;
+    while (*p){
+        while ((USART2->ISR & USART_ISR_TXE) == 0){}
+        USART2->TDR = *p++;
+    }
+    while ((USART2->ISR & USART_ISR_TC) == 0){}
 }
+
