13fa437212a0834ef05cd62a29e2da11c4a13b60
Bryce <bwang56@calpoly.edu>
2025-12-03 23:28:22 -0800

    Added dma

---
+++ inc/dma.h
@@ -0,0 +1,10 @@
+#ifndef DMA_H
+#define DMA_H
+
+#include "stm32l4xx_hal.h" // so we can use HAL functions
+#include <stdint.h>
+
+void DMA_INIT(void);
+
+
+#endif
---
+++ src/dma.c
@@ -0,0 +1,6 @@
+#include "dma.h"
+
+void DMA_INIT(void){
+    
+
+}
