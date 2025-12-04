77eb13b0d3636d0a657aea9bb07be099b5ea094f
ntintran <147439814+ntintran@users.noreply.github.com>
2025-12-03 16:55:37 -0800

    Changed out HAL for typedef enum

--- inc/i2c.h
+++ inc/i2c.h
@@ -5,8 +5,16 @@
 #include "stm32l4xx_hal.h" // so we can use HAL functions
 #include <stdint.h>
 
+typedef enum {
+    I2C_OK = 0,
+    I2C_ERROR,
+    I2C_TIMEOUT_ERR
+} I2C_Status;
+
+#define I2C_TIMEOUT  (100000U)
+
 void I2C_init(void);
-HAL_StatusTypeDef I2C1_read(uint16_t device_address, uint8_t register_address, uint8_t *data, uint16_t size);
-HAL_StatusTypeDef I2C1_write(uint16_t device_address, uint8_t register_address, uint8_t *data, uint16_t size);
+I2C_Status I2C1_read(uint16_t device_address, uint8_t register_address, uint8_t *data, uint16_t size);
+I2C_Status I2C1_write(uint16_t device_address, uint8_t register_address, uint8_t *data, uint16_t size);
 
 #endif
--- src/i2c.c
+++ src/i2c.c
@@ -25,38 +25,38 @@ void I2C1_init(void){
     I2C1->CR1 |= I2C_CR1_PE;
 }
 
-static HAL_StatusTypeDef I2C1_WaitFlagSet(volatile uint32_t *reg, uint32_t flag){
+static I2C_Status I2C1_WaitFlagSet(volatile uint32_t *reg, uint32_t flag){
     uint32_t timeout = I2C_TIMEOUT;
     while(((*reg) & flag) == 0U) {
         if (--timeout == 0U) {
-            return HAL_TIMEOUT;
+            return I2C_TIMEOUT_ERR;
         }
     }
-    return HAL_OK;
+    return I2C_OK;
 }
 
-static HAL_StatusTypeDef I2C1_WaitFlagClear(volatile uint32_t *reg, uint32_t flag){
+static I2C_Status I2C1_WaitFlagClear(volatile uint32_t *reg, uint32_t flag){
     uint32_t timeout = I2C_TIMEOUT;
     while(((*reg) & flag) != 0U) {
         if (--timeout == 0U) {
-            return HAL_TIMEOUT;
+            return I2C_TIMEOUT_ERR;
         }
     }
-    return HAL_OK;
+    return I2C_OK;
 }
 
-HAL_StatusTypeDef I2C1_write(uint16_t device_address, uint8_t register_address, uint8_t *data, uint16_t size){ // writes data, doesnt really get anything back
+I2C_Status I2C1_write(uint16_t device_address, uint8_t register_address, uint8_t *data, uint16_t size){ // writes data, doesnt really get anything back
 	
-    HAL_StatusTypeDef ret;
+    I2C_Status ret;
     if (size == 0) {
-        return HAL_OK;
+        return I2C_OK;
     }
     if (size > 255) {
-        return HAL_ERROR;
+        return I2C_ERROR;
     }
 	
     ret = I2C1_WaitFlagClear(&I2C1->ISR, I2C_ISR_BUSY); // wait until bus is free
-    if (ret != HAL_OK) return ret;
+    if (ret != I2C_OK) return ret;
 	// clear flags
     if (I2C1->ISR & I2C_ISR_NACKF) {
         I2C1->ICR = I2C_ICR_NACKCF;
@@ -73,38 +73,38 @@ HAL_StatusTypeDef I2C1_write(uint16_t device_address, uint8_t register_address, 
     I2C1->CR2 |= I2C_CR2_START;
 
     ret = I2C1_WaitFlagSet(&I2C1->ISR, I2C_ISR_TXIS); // send register address
-    if (ret != HAL_OK) return ret;
+    if (ret != I2C_OK) return ret;
     I2C1->TXDR = register_address;
  
     for (uint16_t i = 0; i < size; i++){ // send data bits
         ret = I2C1_WaitFlagSet(&I2C1->ISR, I2C_ISR_TXIS);
-        if (ret != HAL_OK) return ret;
+        if (ret != I2C_OK) return ret;
         I2C1->TXDR = data[i];
     }
     ret = I2C1_WaitFlagSet(&I2C1->ISR, I2C_ISR_STOPF); // wait
-    if (ret != HAL_OK) return ret;
+    if (ret != I2C_OK) return ret;
 
     I2C1->ICR = I2C_ICR_STOPCF;
 
     if (I2C1->ISR & I2C_ISR_NACKF) {
         I2C1->ICR = I2C_ICR_NACKCF;
-        return HAL_ERROR;
+        return I2C_ERROR;
     }
 
-    return HAL_OK;
+    return I2C_OK;
 }
 
-HAL_StatusTypeDef I2C1_read(uint16_t device_address, uint8_t register_address, uint8_t *data, uint16_t size){
+I2C_Status I2C1_read(uint16_t device_address, uint8_t register_address, uint8_t *data, uint16_t size){
 	
-    HAL_StatusTypeDef ret;
+    I2C_Status ret;
     if (size == 0){
-        return HAL_OK;
+        return I2C_OK;
     }
     if (size > 255){
-        return HAL_ERROR;
+        return I2C_ERROR;
     }
     ret = I2C1_WaitFlagClear(&I2C1->ISR, I2C_ISR_BUSY);
-    if (ret != HAL_OK) return ret;
+    if (ret != I2C_OK) return ret;
 
     // write first
     I2C1->CR2 = 0;
@@ -115,11 +115,11 @@ HAL_StatusTypeDef I2C1_read(uint16_t device_address, uint8_t register_address, u
     I2C1->CR2 |= I2C_CR2_START;
 	
     ret = I2C1_WaitFlagSet(&I2C1->ISR, I2C_ISR_TXIS);
-    if (ret != HAL_OK) return ret;
+    if (ret != I2C_OK) return ret;
     I2C1->TXDR = register_address;
 
     ret = I2C1_WaitFlagSet(&I2C1->ISR, I2C_ISR_TC);
-    if (ret != HAL_OK) return ret;
+    if (ret != I2C_OK) return ret;
 
     // then we read
     I2C1->CR2 &= ~(I2C_CR2_SADD | I2C_CR2_NBYTES | I2C_CR2_RD_WRN);
@@ -132,21 +132,21 @@ HAL_StatusTypeDef I2C1_read(uint16_t device_address, uint8_t register_address, u
 
     for (uint16_t i = 0; i < size; i++){ // actual reading happens here
         ret = I2C1_WaitFlagSet(&I2C1->ISR, I2C_ISR_RXNE);
-        if (ret != HAL_OK) return ret;
+        if (ret != I2C_OK) return ret;
         data[i] = (uint8_t)I2C1->RXDR;
     }
 
     ret = I2C1_WaitFlagSet(&I2C1->ISR, I2C_ISR_STOPF);
-    if (ret != HAL_OK) return ret;
+    if (ret != I2C_OK) return ret;
 
     I2C1->ICR = I2C_ICR_STOPCF;
 	
     if (I2C1->ISR & I2C_ISR_NACKF) {
         I2C1->ICR = I2C_ICR_NACKCF;
-        return HAL_ERROR;
+        return I2C_ERROR;
     }
 
-    return HAL_OK;
+    return I2C_OK;
 }
 
 
