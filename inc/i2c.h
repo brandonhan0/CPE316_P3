507e20aec723996d56550e7b10e8611dcf20e6f1
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2025-12-08 13:07:17 -0800

    Update i2c.h

--- inc/i2c.h
+++ inc/i2c.h
@@ -5,16 +5,8 @@
 #include "stm32l4xx_hal.h" // so we can use HAL functions
 #include <stdint.h>
 
-typedef enum {
-    I2C_OK = 0,
-    I2C_ERROR,
-    I2C_TIMEOUT_ERR
-} I2C_Status;
-
-#define I2C_TIMEOUT  (100000U)
-
-void I2C_init(void);
-I2C_Status I2C1_read(uint16_t device_address, uint8_t register_address, uint8_t *data, uint16_t size);
-I2C_Status I2C1_write(uint16_t device_address, uint8_t register_address, uint8_t *data, uint16_t size);
+static void I2C3_Init(void);
+static void I2C3_Read(uint8_t addr7, uint8_t *data, uint8_t len)
+static void I2C3_Write(uint8_t addr7, const uint8_t *data, uint8_t len);
 
 #endif
