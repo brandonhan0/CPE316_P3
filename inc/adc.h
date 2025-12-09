4afd20587a7c6191140910d7ccf3b2c215bda3e8
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2025-12-09 00:43:39 -0800

    Update adc.h

--- inc/adc.h
+++ inc/adc.h
@@ -2,10 +2,11 @@
 
 #define ADC_H
 
+#include "stm32l476xx.h"
 #include "stm32l4xx_hal.h" // so we can use HAL functions
 #include <stdint.h>
 
-void ADC1_init(void);
+void ADC_init(void);
 uint16_t ADC1_read(void);
 int adc_to_mV(uint16_t data);
 
