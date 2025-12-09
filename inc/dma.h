21e97b4c01c4c0cc55c1edb212558c63ad86a2b7
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2025-12-09 00:43:02 -0800

    Update dma.h

--- inc/dma.h
+++ inc/dma.h
@@ -3,16 +3,14 @@
 
 #include "stm32l4xx_hal.h" // so we can use HAL functions
 #include <stdint.h>
-#include "stm32l4xx.h" 
+#include "stm32l4xx.h"
 
-#define N_SAMPLES  64
 
-volatile uint16_t adc_buf[N_SAMPLES];
-volatile uint8_t  i2c_buf[N_SAMPLES * 2];
-uint8_t usart_tx_buf[N_SAMPLES * 4];
-volatile uint8_t adc_ready = 0;
-volatile uint8_t i2c_ready = 0;
-volatile uint8_t tx_busy   = 0;
+#define N_SAMPLES  1
+
+extern volatile uint8_t tx_busy;
+extern uint8_t usart_tx_buf[N_SAMPLES * 8];
+
 
 void adc_dma_init(void);
 void i2c3_dma_init(void);
