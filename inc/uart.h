b833ec6575f2df7f1664e4405ab17781ad7bdf99
brandonhan0 <145811878+brandonhan0@users.noreply.github.com>
2025-12-10 10:26:23 -0800

    Update uart.h

--- inc/uart.h
+++ inc/uart.h
@@ -1,11 +1,11 @@
-#ifndef UART_H
+#ifndef USART_H
 
-#define UART_H
+#define USART_H
 
 #include "stm32l4xx_hal.h" // so we can use HAL functions
 #include <stdint.h>
 
-void UART2_INIT(uint32_t pclk1_hz, uint32_t baud);
-void UART_write(const char *s);
+void USART2_INIT(void);
+void USART_write(const char *s);
 
 #endif
